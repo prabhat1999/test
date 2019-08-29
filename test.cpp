ac31a8cc9062bdf8b4e0bacb9986d1d9c01cb303
unknown <prabhat.avb@gmail.com>
2019-08-29 21:21:59 +0530

    con

--- test.cpp
+++ test.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main(){
   char name[10]; int age;
-  int a = 15;
+  int a = 25; //made by A1
   cout<< "Enter your name and age"<<endl;
   cin>> name >> age;
   cout<< "Your name is "<<name<<" and you are "<<age<<" years old";
 
   return 0;
 }
+//Changes made by person A1
