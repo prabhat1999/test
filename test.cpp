3cf9022174cd2b812c33fad6a222fb44fbf833da
unknown <prabhat.avb@gmail.com>
2019-08-29 21:13:11 +0530

    conflict

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
   char name[10]; int age;
-  int a = 15;
+  int a = 155;
   cout<< "Enter your name and age"<<endl;
   cin>> name >> age;
   cout<< "Your name is "<<name<<" and you are "<<age<<" years old";
